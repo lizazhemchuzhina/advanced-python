da62c96541468c7b386da675a13b8993030a3221
lizazhemchuzhina <lizazhemchuzhina@mail.ru>
2022-02-14 23:32:39 +0300

    hw 1 fix name

--- hw_01/src/main.py
+++ hw_01/src/main.py
@@ -2,11 +2,11 @@ import ast
 import inspect
 import networkx as nx
 
-from hw_01.lizahwlib.ast_graph import AstGraph
-from hw_01.lizahwlib.fibonacci import fibonacci
+from hw_01.src.ast_graph import AstGraph
+from hw_01.src.fibonacci import fibonacci
 
 if __name__ == "__main__":
     ast_object = ast.parse(inspect.getsource(fibonacci))
     ast_graph = AstGraph()
     ast_graph.visit(ast_object)
-    nx.drawing.nx_pydot.to_pydot(ast_graph.graph).write_png("artifacts/ast.png")
+    nx.drawing.nx_pydot.to_pydot(ast_graph.graph).write_png("hw_01/artifacts/ast.png")
